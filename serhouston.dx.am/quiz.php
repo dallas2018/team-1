79be2c27fab3be237a53641140c263d2581ac3f3
Hepson Sanchez <hepsonms6@gmail.com>
2018-10-06 08:37:52 -0500

    added text message alerts

--- serhouston.dx.am/quiz.php
+++ serhouston.dx.am/quiz.php
@@ -30,6 +30,13 @@
                     </div>
 
             <!--BUttons-->
+            <?php
+            $from='From: Mail Contact Form <team1@serhouston.dx.am>';
+            $to='2148813311@mymetropcs.com';
+            $subject='SER Houston Application';
+            $body='Please do not forget to complete your application on http://serhouston.dx.am/!';
+            #mail($to,$subject,$body,$from);
+            ?>
             <form>
                     <div class="container" id="imgbuttons">
                             <div class="container" id="question">What best represents your living situation?</div>
