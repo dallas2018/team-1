eb0fe79d43bb342b65faa76f45e3b9ddc1cf0e7f
Hepson Sanchez <hepsonms6@gmail.com>
2018-10-06 06:10:00 -0500

    reverted stylesheet

--- serhouston.dx.am/index.php
+++ serhouston.dx.am/index.php
@@ -177,7 +177,6 @@ include "base.php";
 <?php
 function launch_modal()
 {?>
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
   <!-- Modal -->
   <div class="modal fade" id="myModal" role="dialog">
     <div class="modal-dialog">
@@ -205,7 +204,6 @@ function launch_modal()
       </div>
     </div>
   </div>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">  
   <?php
 }
 ?>
