80d4b9498beadaafc8ad292e69a63ad4532b7c40
Hepson Sanchez <hepsonms6@gmail.com>
2018-10-06 03:42:41 -0500

    added php condition to form

--- serhouston.dx.am/index.php
+++ serhouston.dx.am/index.php
@@ -26,84 +26,94 @@
 
           <h2 class="bordering">Contact Form</h2>
           <hr>
-          <p id="contacts">
-            <div class="progress">
-              <div class="progress-bar" role="progressbar" style="width: 10%;" aria-valuenow="25" aria-valuemin="0"
-                aria-valuemax="100">10%</div>
+          <?php
+          if(!empty($_POST['inputFirstName']) && !empty($_POST['inputLastName']) && 
+              !empty($_POST['inputPhoneNumber']) && !empty($_POST['inputZipcode'])) {
+                echo "NICE"
+          }
+          else {
+          ?>
+            <p id="contacts">
+              <div class="progress">
+                <div class="progress-bar" role="progressbar" style="width: 10%;" aria-valuenow="25" aria-valuemin="0"
+                  aria-valuemax="100">10%</div>
+              </div>
+              <form method="post" action="index.php" name="form" id="form">
+                <div class="form-row">
+                  <div class="col-md-4 mb-3">
+                    <label for="inputFirstName">First Name</label><span class="error"> * </span>
+                    <input type="text" class="form-control" id="inputFirstName" placeholder="First Name" required>
+                    <div class="valid-tooltip">
+                      Looks good!
+                    </div>
+                  </div>
+                  <div class="col-md-4 mb-3">
+                    <label for="inputMiddleName">Middle Name</label>
+                    <input type="text" class="form-control" id="inputMiddleName" placeholder="Middle Name">
+                    <div class="valid-tooltip">
+                      Looks good!
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-4 mb-3">
+                  <label for="inputLastName">Last Name</label><span class="error"> * </span>
+                  <input type="text" class="form-control" id="inputLastName" placeholder="Last Name" required>
+                  <div class="valid-tooltip">
+                    Looks good!
+                  </div>
+                </div>
+                <div class="form-row">
+                  <div class="col-md-6 mb-3">
+                    <label for="validationTooltip03">City</label>
+                    <input type="text" class="form-control" id="validationTooltip03" placeholder="City">
+                    <div class="invalid-tooltip">
+                      Please provide a valid city.
+                    </div>
+                  </div>
+                  <div class="col-md-3 mb-3">
+                    <label for="validationTooltip04">State</label>
+                    <input type="text" class="form-control" id="validationTooltip04" placeholder="State">
+                    <div class="invalid-tooltip">
+                      Please provide a valid state.
+                    </div>
+                  </div>
+                  <div class="col-md-3 mb-3">
+                    <label for="inputZipcode">Zip</label></label><span class="error"> * </span>
+                    <input type="text" class="form-control" id="inputZipcode" placeholder="Zip" required>
+                    <div class="invalid-tooltip">
+                      Please provide a valid zip.
+                    </div>
+                  </div>
+                  <div class="col-md-4 mb-3">
+                    <label for="inputPhoneNumber">Phone number</label><span class="error"> * </span>
+                    <input type="tel" class="form-control" id="inputPhoneNumber" placeholder="111-1111-1111" required>
+                    <div class="invalid-tooltip">
+                      Please provide a valid cell phone number.
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-4 mb-3">
+                  <label for="validationTooltip02">County</label>
+                  <input type="text" class="form-control" id="validationTooltip05" placeholder="Brazoria">
+                  <div class="invalid-tooltip">
+                    Please provide a County.
+                  </div>
+                </div>
+          </div>
+          <div class="col-md-4 mb-3">
+            <label for="validationTooltip02">Date of Birth</label>
+            <input type="date" class="form-control" id="validationTooltip05" placeholder="MM/DD/YYYY" required>
+            <div class="invalid-tooltip">
+              Please provide a County.
             </div>
-            <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" name="form" id="form">
-              <div class="form-row">
-                <div class="col-md-4 mb-3">
-                  <label for="inputFirstName">First Name</label><span class="error"> * </span>
-                  <input type="text" class="form-control" id="inputFirstName" placeholder="First Name" required>
-                  <div class="valid-tooltip">
-                    Looks good!
-                  </div>
-                </div>
-                <div class="col-md-4 mb-3">
-                  <label for="inputMiddleName">Middle Name</label>
-                  <input type="text" class="form-control" id="inputMiddleName" placeholder="Middle Name">
-                  <div class="valid-tooltip">
-                    Looks good!
-                  </div>
-                </div>
-              </div>
-              <div class="col-md-4 mb-3">
-                <label for="inputLastName">Last Name</label><span class="error"> * </span>
-                <input type="text" class="form-control" id="inputLastName" placeholder="Last Name" required>
-                <div class="valid-tooltip">
-                  Looks good!
-                </div>
-              </div>
-              <div class="form-row">
-                <div class="col-md-6 mb-3">
-                  <label for="validationTooltip03">City</label>
-                  <input type="text" class="form-control" id="validationTooltip03" placeholder="City">
-                  <div class="invalid-tooltip">
-                    Please provide a valid city.
-                  </div>
-                </div>
-                <div class="col-md-3 mb-3">
-                  <label for="validationTooltip04">State</label>
-                  <input type="text" class="form-control" id="validationTooltip04" placeholder="State">
-                  <div class="invalid-tooltip">
-                    Please provide a valid state.
-                  </div>
-                </div>
-                <div class="col-md-3 mb-3">
-                  <label for="validationTooltip05">Zip</label></label><span class="error"> * </span>
-                  <input type="text" class="form-control" id="validationTooltip05" placeholder="Zip" required>
-                  <div class="invalid-tooltip">
-                    Please provide a valid zip.
-                  </div>
-                </div>
-                <div class="col-md-4 mb-3">
-                  <label for="inputPhoneNumber">Phone number</label><span class="error"> * </span>
-                  <input type="tel" class="form-control" id="inputPhoneNumber" placeholder="111-1111-1111" required>
-                  <div class="invalid-tooltip">
-                    Please provide a valid cell phone number.
-                  </div>
-                </div>
-              </div>
-              <div class="col-md-4 mb-3">
-                <label for="validationTooltip02">County</label>
-                <input type="text" class="form-control" id="validationTooltip05" placeholder="Brazoria">
-                <div class="invalid-tooltip">
-                  Please provide a County.
-                </div>
-              </div>
-        </div>
-        <div class="col-md-4 mb-3">
-          <label for="validationTooltip02">Date of Birth</label>
-          <input type="date" class="form-control" id="validationTooltip05" placeholder="MM/DD/YYYY" required>
-          <div class="invalid-tooltip">
-            Please provide a County.
           </div>
         </div>
-      </div>
-      <button class="btn btn-primary" type="submit">Submit form</button>
-      </form>
-      </p>
+        <button class="btn btn-primary" type="submit">Submit form</button>
+        </form>
+        </p>
+        <?php
+      }
+      ?>
     </div>
   </div>
   </div>
